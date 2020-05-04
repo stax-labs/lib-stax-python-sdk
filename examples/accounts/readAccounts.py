d9b1b7c2c3e37c42571740f8b4df8feb978f7f0e
Mat Lord <mat.lord@versent.com.au>
2020-05-04 11:58:57 +1000

    chore(examples): Set example directory structure

---
+++ examples/accounts/readAccounts.py
@@ -0,0 +1,17 @@
+import os
+
+from stax.config import Config
+from stax.openapi import StaxClient
+
+Config.access_key = os.getenv("STAX_ACCESS_KEY")
+Config.secret_key = os.getenv("STAX_SECRET_KEY")
+
+# Read all accounts within your Stax Organisation
+accounts = StaxClient("accounts")
+response = accounts.ReadAccounts()
+print(response)
+
+# Read all active accounts within your Stax Organisation and include tags in the response
+accounts = StaxClient("accounts")
+response = accounts.ReadAccounts(filter="ACTIVE", include_tags=True)
+print(response)
