62653c7253338f3aa621b4e934b6ab15ef773a87
Dean <dean.shea@hotmail.com>
2020-05-11 10:18:11 +1000

    feat(api-token) example of accessing api_tokens from python

---
+++ examples/api_tokens/get_api_token.py
@@ -0,0 +1,24 @@
+import boto3
+import sys
+import os
+
+# The name of the api token
+api_token_name = <Api Token Name>
+# The security account id
+security_account = <Security Account AWS Id>
+# The account that can access the security tokens that you are logged in as
+access_account = <Account AWS Id>
+
+sts = boto3.client("sts")
+
+response = sts.assume_role(RoleArn=f"arn:aws:iam::{security_account}:role/{api_token_name}-access-role", RoleSessionName=f"{api_token_name}-ssm-role")
+assumed_ssm = boto3.client("ssm",
+aws_access_key_id=response["Credentials"]["AccessKeyId"],
+aws_secret_access_key=response["Credentials"]["SecretAccessKey"],
+aws_session_token=response["Credentials"]["SessionToken"]
+)
+api_token_access_key = assumed_ssm.get_parameter(Name=f"/stax/api-tokens/{api_token_name}/AccessKey", WithDecryption=True)
+api_token_secret_key = assumed_ssm.get_parameter(Name=f"/stax/api-tokens/{api_token_name}/SecretKey", WithDecryption=True)
+print(f"Access Key: {api_token_access_key['Parameter']['Value']}")
+print(f"Secret Key: {api_token_secret_key['Parameter']['Value']}")
+
