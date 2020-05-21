6024e788c8d28ca9c9a4cbaabddf633d851bc62b
Dean <dean.shea@hotmail.com>
2020-05-21 16:08:58 +1000

    feat(JUMA-3378) Add examples for groups

---
+++ examples/groups/update_group_members.py
@@ -0,0 +1,22 @@
+import os
+
+from staxapp.config import Config
+from staxapp.openapi import StaxClient
+
+Config.access_key = os.getenv("STAX_ACCESS_KEY")
+Config.secret_key = os.getenv("STAX_SECRET_KEY")
+
+# An array of user memberships dicts to enableor an empty array
+# 	Properties: GroupId(string), UserId(string)
+user_membership_to_add = <An array of User Membership?>
+
+# An array of user memberships dicts to disable or an empty arry
+user_membership_to_remove = <An array of User Membership?>
+
+# Create a Stax groups
+teams = StaxClient("teams")
+response = teams.UpdateGroupMembers(
+	AddMembers = user_membership_to_add,
+	RemoveMembers = user_membership_to_remove
+)
+print(response)
