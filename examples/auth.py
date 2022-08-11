b798c43292330d7fcb0c0716144786d9b6f4b666
Dean Shea <dean.shea@hotmail.com>
2022-08-11 10:37:04 +1000

    chore(sdk): add doco
    add an example for how to use a config client

---
+++ examples/auth.py
@@ -0,0 +1,33 @@
+import json
+import os
+
+from staxapp.config import Config
+from staxapp.openapi import StaxClient
+from customscript import get_hostnames
+hostname = get_hostnames()
+
+access_key = os.getenv("STAX_ACCESS_KEY")
+secret_key = os.getenv("STAX_SECRET_KEY")
+
+Config.hostname = hostname["au1"]
+Config.access_key = access_key
+Config.secret_key = secret_key
+
+accounts_au1 = StaxClient('accounts')
+
+au1_response = accounts_au1.CreateAccountType(
+	Name="sdk-au1"
+)
+
+print(json.dumps(au1_response, indent=4, sort_keys=True))
+
+access_key_2 = os.getenv("STAX_ACCESS_KEY_2")
+secret_key_2 = os.getenv("STAX_SECRET_KEY_2")
+config = Config(hostname=hostname["us1"], access_key=access_key_2, secret_key=secret_key_2)
+
+us1_accounts = StaxClient('accounts', config=config)
+
+us1_response = us1_accounts.CreateAccountType(
+	Name="sdk-us1"
+)
+print(json.dumps(us1_response, indent=4, sort_keys=True))
