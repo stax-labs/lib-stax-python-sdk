da48811bca466e8d73414ec4dece9c9bba2b5193
Levi Quinn <levi@stax.io>
2024-05-30 10:42:37 +1000

    docs(platform): account example orgid

--- examples/accounts/create_accounts.py
+++ examples/accounts/create_accounts.py
@@ -6,6 +6,7 @@ from staxapp.openapi import StaxClient
 
 account_name = <Account Name>
 account_type = <Account Type Id>
+organisational_unit_id = <Organisational Unit Id>
 
 Config.access_key = os.getenv("STAX_ACCESS_KEY")
 Config.secret_key = os.getenv("STAX_SECRET_KEY")
@@ -15,5 +16,6 @@ accounts = StaxClient('accounts')
 response = accounts.CreateAccount(
 		Name=account_name,
 		AccountType=account_type,
+        OrganisationalUnitId=organisational_unit_id,
 )
 print(json.dumps(response, indent=4, sort_keys=True))
